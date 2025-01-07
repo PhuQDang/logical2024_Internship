529d5bbf537e1fe5bed06ba61eec1e27b2ec4a54
PhuQDang <pdang10102005@gmail.com>
2025-01-07 15:03:57 +0700

    created a script to sort businesses into their respective industrial zones (if applicable)

---
+++ industrial_zone.py
@@ -0,0 +1,158 @@
+import pandas as pd
+import psycopg2
+from psycopg2 import sql
+from sqlalchemy import create_engine
+import numpy as np
+import logging
+import sys
+from typing import Dict, List, Tuple
+import re
+
+class IndustrialZoneBusinessesFinder:
+    def __init__(self, db_params: dict[str, str], excel_file = None) -> None:
+        self.db_params = db_params
+        self.excel_file = excel_file
+        self.setup_logging()
+    
+    def setup_logging(self):
+        logging.basicConfig(
+            level=logging.INFO,
+            format='%(asctime)s - %(levelname)s - %(message)s',
+            handlers=[
+                logging.FileHandler('industrial_filter.log'),
+                logging.StreamHandler(sys.stdout)
+            ]
+        )
+        self.logger = logging.getLogger(__name__)
+
+    def create_industrial_schema(self) -> None:
+        try:
+            conn = psycopg2.connect(**self.db_params)
+            cur = conn.cursor()
+
+            schema_sql = """
+            CREATE TABLE IF NOT EXISTS industrial_zones (
+                id      SERIAL PRIMARY KEY,
+                name    varchar(255) UNIQUE
+            );
+            CREATE TABLE IF NOT EXISTS industrial_zone_businesses (
+                business_id     int REFERENCES general_businesses(id),
+                zone_id         int REFERENCES industrial_zones(id),
+                PRIMARY KEY (business_id, zone_id)
+            );
+            """
+            cur.execute(schema_sql)
+            conn.commit()
+            self.logger.info("Schema created successfully")
+        except Exception as e:
+            conn.rollback()
+            self.logger.error(f"Error creating schema: {str(e)}")
+            raise
+        finally:
+            if conn:
+                cur.close()
+                conn.close()
+
+    def process_industrial_zones(self) -> dict[int, str]:
+        """
+        Extract unique industrial zones and create a map of zone's name and zone's id
+        """
+        conn = psycopg2.connect(**self.db_params)
+        cur = conn.cursor()
+
+        cur.execute("SELECT id, address FROM general_businesses")
+        id_addresses = cur.fetchall()
+        industrialZoneMap = {}
+        idZoneMap = {}
+
+        def extract_industrial_zone(address: str) -> str:
+            addressProcessed = re.search(r"(KCN)\D+", address)
+            if addressProcessed != None:
+                addressProcessed = re.findall(r"[^–()-]+", addressProcessed.group().split(',')[0].upper())
+                for i in range(len(addressProcessed)):
+                    addressProcessed[i] = addressProcessed[i].strip()
+                addressProcessed = ' '.join(addressProcessed)
+            return addressProcessed
+        
+        data = {"industrial_zone" : []}
+        for id, address in id_addresses:
+            industrialZone = extract_industrial_zone(address)
+            if industrialZone == None:
+                continue
+            idZoneMap[id] = industrialZone
+            data["industrial_zone"].append(industrialZone)
+        try:
+            df = pd.DataFrame(data)['industrial_zone'].unique()
+            for zone in df:
+                cur.execute(
+                    """
+                    WITH e AS (
+                    INSERT INTO industrial_zones (name)
+                    VALUES
+                        (%s)
+                    ON CONFLICT DO NOTHING
+                    RETURNING id
+                    )
+                    SELECT * FROM e
+                    UNION
+                        SELECT id FROM industrial_zones WHERE name = %s
+                    """, (zone, zone)
+                )
+                zoneId = cur.fetchone()[0]
+                industrialZoneMap[zone] = zoneId
+            conn.commit()
+            return industrialZoneMap, idZoneMap
+        
+        except Exception as e:
+            cur.rollback()
+            self.logger.error(f"Error extracting industrial zones from address {str(e)}")
+            raise
+        finally:
+            if conn:
+                cur.close()
+                conn.close()
+            print("Finish processing addresses and industrial zoness")
+            
+    def address_to_zone(self) -> None:
+        conn = psycopg2.connect(**self.db_params)
+        cur = conn.cursor()
+        try:
+            industrialZoneMap, idZoneMap = self.process_industrial_zones()
+            for business_id, zone in idZoneMap.items():
+                cur.execute(
+                    """
+                    INSERT INTO industrial_zone_businesses (business_id, zone_id)
+                    VALUES
+                        (%s, %s)
+                    ON CONFLICT DO NOTHING
+                    """, (business_id, industrialZoneMap[zone])
+                )
+            conn.commit()
+        except Exception as e:
+            cur.rollback()
+            self.logger.error(str(e))
+        finally:
+            if conn:
+                cur.close()
+                conn.close()
+            print("Finish filtering businesses")
+
+def main():
+    db_params = {
+        'host': 'localhost',
+        'database': 'businessesdb',
+        'user': 'postgres',
+        'password': '1234',
+        'port': '5432'
+    }
+    industrialFilter = IndustrialZoneBusinessesFinder(db_params)
+    try:
+        industrialFilter.setup_logging()
+        industrialFilter.create_industrial_schema()
+        industrialFilter.address_to_zone()
+    except Exception as e:
+        print(str(e))
+        sys.exit(1)
+
+if __name__ == "__main__":
+    main()
--- main.py
+++ main.py
@@ -1,4 +1,30 @@
-import general_database
+from general_database import *
+from industrial_zone import *
+
+def general_setup(fname, industrialSetUp = True):
+    db_params = {
+        'host': 'localhost',
+        'database': 'businessesdb',
+        'user': 'postgres',
+        'password': '1234',
+        'port': '5432'
+    }
+    excel_file = fname
+    
+    # Create importer and run import
+    importer = VNBusinessImporter(db_params, excel_file)
+    
+    try:
+        importer.import_data()
+        print("Data import completed successfully!")
+        if industrialSetUp:
+            industrialFilter = IndustrialZoneBusinessesFinder(db_params)
+            industrialFilter.setup_logging()
+            industrialFilter.create_industrial_schema()
+            industrialFilter.address_to_zone()
+    except Exception as e:
+        print(f"Error: {str(e)}")
+        return
 
 def main():
     while True:
@@ -10,12 +36,21 @@ def main():
             if not 0 <= int(option) <= 2:
                 print("Invalid option")
                 continue
-            
+            elif int(option) == 0:
+                sys.exit(0)
+            elif int(option) == 1:
+                ...
+            elif int(option) == 2:
+                ...
+
         except KeyboardInterrupt:
-            return
-        except:
+            sys.exit(0)
+        except ValueError:
             print("Invalid option")
             continue
+        except Exception as e:
+            print(f"Error: {str(e)}")
+            sys.exit(1)
 
 if __name__ == "__main__":
     main()
