e1f09e1c92e33ffb6e29418ab5558f425e3f8942
PhuQDang <pdang10102005@gmail.com>
2025-02-06 21:14:23 +0700

    final changes

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from general_database import *
 from query_functions import *
 
-def general_setup(fname):
+def general_setup(fname) -> PotentialCustomers:
     db_params = {
         'host': 'localhost',
         'database': 'businessesdb',
@@ -17,7 +17,6 @@ def general_setup(fname):
     try:
         importer.import_data()
         print("Data import completed successfully!")
-        classifier = PotentialCustomers(db_params)
     except Exception as e:
         print(f"Error: {str(e)}")
         return
@@ -34,9 +33,10 @@ def main():
         try:
             option = input("1. Read excel file\n"
                            "2. Query database\n"
+                           "3. List of potential customers\n"
                            "0. Exit\n"
                            "Option: ")
-            if not 0 <= int(option) <= 2:
+            if not 0 <= int(option) <= 3:
                 print("Invalid option")
                 continue
             elif int(option) == 0:
@@ -45,8 +45,16 @@ def main():
                 fpath = input()
                 general_setup(fname=fpath)
             elif int(option) == 2:
-                queryRespond = QueryPrompter(db_parms=db_params)
+                queryRespond = QueryPrompter(db_params=db_params)
                 queryRespond.query_results()
+            elif int(option) == 3:
+                custClassifier = PotentialCustomers(db_params=db_params)
+                targetCost = input("Enter the minimum charter capital of the company (default = 3e9 VND): ")
+                if targetCost.strip() == "":
+                    print(custClassifier.classify())
+                else:
+                    targetCost = int(targetCost)
+                    print(custClassifier.classify(targetCost=targetCost))
         except KeyboardInterrupt:
             sys.exit(0)
         except ValueError:
@@ -57,8 +65,9 @@ def main():
             sys.exit(1)
 
 if __name__ == "__main__":
-    fname = 'dsdn_1997_2024_processed.xlsx'
-    start = time.time()
-    general_setup(fname)
-    end = time.time()
-    print(f"Time taken to import all data = {end-start}")
+    # fname = 'dsdn_1997_2024_processed.xlsx'
+    # start = time.time()
+    # general_setup(fname)
+    # end = time.time()
+    # print(f"Time taken to import all data = {end-start}")
+    main()
